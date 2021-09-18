393f845a63032ccdb76a97fd57ffc3f32a116ece
Павлов Александр <alexandr.pavlov@yahoo.com>
2021-09-18 18:46:24 +0300

    go

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
-$(document).ready(function() { 
-	
-	$(".popup_bg").click(function(){	  
-		$(".popup").fadeOut(800);	
-	});
+$(document).ready(function() {
+
+    $(".popup_bg").click(function() {
+        $(".popup").fadeOut(800);
+    });
 
 });
 
 function showPopup() {
-	$(".popup").fadeIn(800); 
+    $(".popup").fadeIn(800);
 }
